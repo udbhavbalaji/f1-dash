203c7efd88b9efb7ef1da826db8f1ee61626cbbb
udbhavbalaji <udbhavbalaji@gmail.com>
2024-10-03 14:39:55 -0400

    Removed unused function

--- front-end/src/services/format.ts
+++ front-end/src/services/format.ts
@@ -1,30 +1,7 @@
-import { Standing } from "@app-types/back-end/ergastResponse";
 import {
-  ConstructorStandingItem,
   ScheduleCardItemType,
   ScheduleItem,
-  StandingItem,
 } from "@app-types/front-end/display";
-
-export const getStandings = (standings: Standing): StandingItem[] => {
-  let output: StandingItem[] = [];
-  console.log(standings);
-  if ("ConstructorStandings" in standings) {
-    const data = standings.ConstructorStandings;
-    for (let i = 0; i < data.length; i++) {
-      const standing: StandingItem = {
-        StandingType: "Constructor",
-        ConstructorStandingItem: {
-          points: parseInt(data[i].points),
-          position: parseInt(data[i].position),
-          ConstructorName: data[i].Constructor.name,
-        },
-      };
-      output.push(standing);
-    }
-  }
-  return output;
-};
 
 export const getScheduleCard = (item: ScheduleItem): ScheduleCardItemType => {
   const cardItem: ScheduleCardItemType = {
